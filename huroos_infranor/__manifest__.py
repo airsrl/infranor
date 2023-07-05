c128d6543175c0e9197eb763389ad78ddec585c0
huroos-frussen <f.frusone@huroos.com>
2023-07-05 17:49:30 +0200

    [DUM] dummy push

--- huroos_infranor/__manifest__.py
+++ huroos_infranor/__manifest__.py
@@ -7,6 +7,7 @@
     'website': "https://www.huroos.com/",
     'category': 'Sales',
     'version': '16.0.0.1',
+
     # any module necessary for this one to work correctly
     'depends': ['base','sale_management','purchase'],
     'data': [
