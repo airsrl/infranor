65e8732127e27cb08029ffaa79c380cf3de73e27
yamil <yamilgiralda@gmail.com>
2023-09-21 16:05:38 +0200

    [UPD] serial number

--- huroos_infranor/models/stock_delivery_note_line.py
+++ huroos_infranor/models/stock_delivery_note_line.py
@@ -4,17 +4,22 @@ from odoo import api, fields, models
 class StockDeliveryNoteLine(models.Model):
     _inherit = "stock.delivery.note.line"
 
+    @api.model
+    def _get_serial_default(self):
+        return {'name': 'hello'}
+
     serial_ids = fields.Many2many(
         "stock.lot",
         string="Numeri di Serie",
+        default=_get_serial_default
     )
 
-    @api.model
-    def _prepare_detail_lines(self, moves):
-        lines = super(StockDeliveryNoteLine, self)._prepare_detail_lines(moves)
-        for line in lines:
-            if 'move_id' in line and line['move_id']:
-                move_id = self.env["stock.move"].browse(line['move_id'])
-                line['serial_ids'] = move_id.lot_ids.ids
-
-        return lines
+    # @api.model
+    # def _prepare_detail_lines(self, moves):
+    #     lines = super(StockDeliveryNoteLine, self)._prepare_detail_lines(moves)
+    #     for line in lines:
+    #         if 'move_id' in line and line['move_id']:
+    #             move_id = self.env["stock.move"].browse(line['move_id'])
+    #             line['serial_ids'] = move_id.lot_ids.ids
+    #
+    #     return lines
