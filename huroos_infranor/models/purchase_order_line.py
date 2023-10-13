6bdd275263558f913e883bbff3c5a60aba0e45f2
kevin-huroos <kevin.poli@air-srl.com>
2023-10-13 14:19:57 +0200

    [upd] huroos_infranor

--- huroos_infranor/models/purchase_order_line.py
+++ huroos_infranor/models/purchase_order_line.py
@@ -4,9 +4,9 @@ from odoo import fields, models, api
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    qty_to_deliver = fields.Float(compute="_compute_qty_to_deliver", store=True, readonly=True)
+    qty_to_deliver = fields.Float(compute="_compute_qty_delivery", store=True, readonly=True)
 
     @api.depends('product_qty', 'qty_received')
-    def _compute_qty_to_deliver(self):
+    def _compute_qty_delivery(self):
         for line in self:
             line.qty_to_deliver = line.product_qty - line.qty_received
--- huroos_infranor/models/sale_order_line.py
+++ huroos_infranor/models/sale_order_line.py
@@ -12,10 +12,10 @@ class SaleOrder(models.Model):
 
     #def compute_previous_invoiced(self):
 
-    qty_to_deliver = fields.Float(compute="_compute_qty_to_deliver", store=True, readonly=True)
+    qty_to_deliver = fields.Float(compute="_compute_qty_delivery", store=True, readonly=True)
 
     @api.depends('product_uom_qty', 'qty_delivered')
-    def _compute_qty_to_deliver(self):
+    def _compute_qty_delivery(self):
         for line in self:
             line.qty_to_deliver = line.product_uom_qty - line.qty_delivered
 
