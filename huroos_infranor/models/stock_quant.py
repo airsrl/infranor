18a92ef194081eb30e0e029e9533268f41940d4b
yamil <yamilgiralda@gmail.com>
2025-05-27 14:54:53 +0200

    [UPD] _get_inventory_fields_create

--- huroos_infranor/models/stock_quant.py
+++ huroos_infranor/models/stock_quant.py
@@ -1,7 +1,13 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class StockQuant(models.Model):
     _inherit = "stock.quant"
 
     price = fields.Float(string="Prezzo")
+
+    @api.model
+    def _get_inventory_fields_create(self):
+        res = super(StockQuant, self)._get_inventory_fields_create()
+        res = res.append('price')
+        return res
