b65fb456dfa94591704ba016e9dbdcb5f8bd5e2d
yamil <yamilgiralda@gmail.com>
2025-05-27 17:35:15 +0200

    [UPD] _unlink_except_done_or_cancel

--- huroos_infranor/models/stock_move_line.py
+++ huroos_infranor/models/stock_move_line.py
@@ -1,11 +1,11 @@
-# from odoo import models, api
-#
-#
-# class StockMoveLine(models.Model):
-#     _inherit = "stock.move.line"
-#
-#     @api.ondelete(at_uninstall=False)
-#     def _unlink_except_done_or_cancel(self):
-#         for ml in self:
-#             if ml.state in ('done', 'cancel'):
-#                 print('You can not delete product moves if the picking is done. You can only correct the done quantities.')
+from odoo import models, api
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    @api.ondelete(at_uninstall=False)
+    def _unlink_except_done_or_cancel(self):
+        for ml in self:
+            if ml.state in ('done', 'cancel'):
+                print('You can not delete product moves if the picking is done. You can only correct the done quantities.')
